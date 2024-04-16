e5357a065112153b517a1796403dd164a3d6346a
sunnykumar07 <sunnyjdk11@gmail.com>
2024-04-17 04:04:27 +0530

    basic layout done here

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/header";
+import Footer from "./Footer/footer";
+import Home from "./Home/home";
+import Contact from "./ContactUs/contact";
+import About from "./AboutUs/about";
+import Github from "./Github/github";
+
+export {Header, Footer, Home, Contact, About, Github}
---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import { Home, About, Contact, Github } from './components/index.js'
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path: "about",
+//         element: <About/>
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact/>
+//       },
+//       {
+//         path: "github",
+//         element: <Github/>
+//       },
+//     ]
+//   }
+// ])
+
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+      <Route path="" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="github" element={<Github/>}/>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
